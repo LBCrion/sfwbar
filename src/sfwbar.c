cd50bd208cf64aaa3d87b813e37c7bf47b5dcb29
Lev Babiev <harley@hosers.org>
2022-10-28 07:45:42 +0100

    fix pointer casting for changes to bar api

--- src/sfwbar.c
+++ src/sfwbar.c
@@ -131,7 +131,7 @@ static void activate (GtkApplication* app, gpointer data )
       css_widget_cascade(GTK_WIDGET(iter->data),NULL);
       base_widget_autoexec(iter->data,NULL);
       if(monitor)
-        bar_set_monitor(monitor,GTK_WIDGET(iter->data));
+        bar_set_monitor(monitor,GTK_WINDOW(iter->data));
     }
   g_list_free(clist);
 
