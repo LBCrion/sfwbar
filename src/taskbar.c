013c8de2ad684a0324625cd68527f89eadfeae3f
Lev Babiev <harley@hosers.org>
2023-11-10 07:46:23 +0000

    taskbar: remove unnecessary taskbar removal for nested bars

--- src/taskbar.c
+++ src/taskbar.c
@@ -229,10 +229,7 @@ void taskbar_destroy_item ( window_t *win )
     {
       flow_grid_delete_child(taskbar, win);
       if(!flow_grid_n_children(taskbar) && taskbar!=iter->data)
-      {
-        taskbars = g_list_remove(taskbars, taskbar);
         flow_grid_delete_child(iter->data, taskbar_group_id(iter->data, win));
-      }
       taskbar_invalidate_item(win);
     }
 }
