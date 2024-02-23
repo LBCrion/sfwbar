07b649d2096fcd61cfdae4474799ce0b602e9bf4
Lev Babiev <harley@hosers.org>
2024-02-23 18:38:42 +0000

    appinfo: correct declaration

--- src/appinfo.h
+++ src/appinfo.h
@@ -2,7 +2,7 @@
 #define __APPINFO_H__
 
 
-void app_info_monitor_init ( void );
+void app_info_init ( void );
 gchar *app_info_icon_get ( const gchar *app_id, GtkIconTheme *theme );
 gchar *app_info_icon_lookup ( gchar *app_id, gboolean prefer_symbolic );
 
