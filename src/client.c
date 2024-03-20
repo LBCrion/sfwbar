328e1ca619c0de4f1f6e32247fd680711c1214fe
LBCrion <76477076+LBCrion@users.noreply.github.com>
2024-03-20 07:51:41 +0000

    use %zu for gsize

--- src/client.c
+++ src/client.c
@@ -50,12 +50,12 @@ gboolean client_event ( GIOChannel *chan, GIOCondition cond, gpointer data )
     }
     if(cstat == G_IO_STATUS_ERROR || !size )
     {
-      g_debug("client %s: read error, status = %d, size = %u",
+      g_debug("client %s: read error, status = %d, size = %zu",
           client->file->fname,cstat,size);
       return FALSE;
     }
     else
-      g_debug("client %s: status %d, read %u bytes",
+      g_debug("client %s: status %d, read %zu bytes",
           client->file->fname,cstat,size);
   }
   if(client->respond)
