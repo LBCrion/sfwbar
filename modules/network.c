61e21170f9c64a622f12e87ac4264dfcec74a6de
Lev Babiev <harley@hosers.org>
2024-07-20 14:36:59 +0100

    modules/network: add routing data to debug info

--- modules/network.c
+++ modules/network.c
@@ -304,7 +304,8 @@ static gboolean net_rt_parse (GIOChannel *chan, GIOCondition cond, gpointer d)
         !g_strcmp0(route->name,if_indextoname(
             ((struct ifinfomsg *)NLMSG_DATA(hdr))->ifi_index,ifname)))
     {
-      g_debug("netinfo: delete default gw iface: %s", route?route->name:"?");
+      g_debug("netinfo: delete default gw iface: %s (%x %x)",
+            route?route->name:"?", gate.s_addr, dest.s_addr);
       net_set_interface(0,gate,gate6);
     }
     else if(hdr->nlmsg_type == RTM_NEWLINK)
@@ -342,13 +343,14 @@ static gboolean net_rt_parse (GIOChannel *chan, GIOCondition cond, gpointer d)
           iidx && !(((struct rtmsg *)NLMSG_DATA(hdr))->rtm_dst_len))
       {
         net_set_interface(iidx, gate,gate6);
-        g_debug("netinfo: new default gw route on: %s", route?route->name:"?");
+        g_debug("netinfo: new default gw route on: %s (%x %x)",
+            route?route->name:"?", gate.s_addr, dest.s_addr);
       }
       else if(hdr->nlmsg_type==RTM_DELROUTE && !dest.s_addr && iidx &&
           !(((struct rtmsg *)NLMSG_DATA(hdr))->rtm_dst_len))
       {
-        g_debug("netinfo: delete default gw route on: %s",
-            route?route->name:"?");
+        g_debug("netinfo: delete default gw route on: %s (%x %x)",
+            route?route->name:"?", gate.s_addr, dest.s_addr);
         net_set_interface(0, gate,gate6);
       }
     }
