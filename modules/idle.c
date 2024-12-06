d6bcd6d986819309b5b79ce50f2c72a302ae91c8
Lev Babiev <harley@hosers.org>
2024-12-06 18:18:40 +0000

    modules/idle: migrate actions to new api

--- modules/idle.c
+++ modules/idle.c
@@ -11,6 +11,7 @@
 #include "ext-idle-notify-v1.h"
 #include "trigger.h"
 #include "util/string.h"
+#include "vm/vm.h"
 
 gint64 sfwbar_module_signature = 0x73f4d956a1;
 guint16 sfwbar_module_version = 2;
@@ -50,36 +51,24 @@ void idle_notification_free ( idle_notification_t *notif )
   g_free(notif);
 }
 
-gboolean sfwbar_module_init ( void )
-{
-  idle_notifier = wayland_iface_register(
-          ext_idle_notifier_v1_interface.name, 1, 1,
-          &ext_idle_notifier_v1_interface);
-  if(!idle_notifier)
-    return FALSE;
-  idle_timers = g_hash_table_new_full((GHashFunc)str_nhash, (GCompareFunc)str_nequal,
-      (GDestroyNotify)idle_notification_free, NULL);
-  return TRUE;
-}
-
-static void idle_timeout_action ( gchar *stime, gchar *name, void *widget,
-    void *d2, void *d3, void *d4 )
+static value_t idle_timeout_action ( vm_t *vm, value_t p[], gint np )
 {
   struct ext_idle_notification_v1 *notif;
   idle_notification_t *timer;
   gint64 new_timeout;
 
-  if(!stime || !name)
-    return;
+  vm_param_check_np(vm, np, 2, "IdleTimeout");
+  vm_param_check_string(vm, p, 0, "IdleTimeout");
+  vm_param_check_string(vm, p, 1, "IdleTimeout");
 
-  new_timeout = g_ascii_strtoll(stime, NULL, 10) * 1000;
+  new_timeout = g_ascii_strtoll(value_get_string(p[0]), NULL, 10) * 1000;
 
-  if( (timer = g_hash_table_lookup(idle_timers, name)) &&
+  if( (timer = g_hash_table_lookup(idle_timers, value_get_string(p[1]))) &&
       (timer->timeout != new_timeout)) 
-    g_hash_table_remove(idle_timers, name);
+    g_hash_table_remove(idle_timers, value_get_string(p[1]));
 
   if(new_timeout < 1)
-    return;
+    return value_na;
 
   if( (notif = ext_idle_notifier_v1_get_idle_notification(
         idle_notifier, new_timeout, gdk_wayland_seat_get_wl_seat(
@@ -87,19 +76,24 @@ static void idle_timeout_action ( gchar *stime, gchar *name, void *widget,
   {
     timer = g_malloc0(sizeof(idle_notification_t));
     timer->timeout = new_timeout;
-    timer->trigger = g_strdup(name);
+    timer->trigger = g_strdup(value_get_string(p[1]));
     timer->notif = notif;
     ext_idle_notification_v1_add_listener(notif, &idle_listener, timer->trigger);
     g_hash_table_insert(idle_timers, timer->trigger, timer);
   }
+
+  return value_na;
 }
 
-static ModuleActionHandlerV1 act_handler1 = {
-  .name = "IdleTimeout",
-  .function = idle_timeout_action
-};
-
-ModuleActionHandlerV1 *sfwbar_action_handlers[] = {
-  &act_handler1,
-  NULL
-};
+gboolean sfwbar_module_init ( void )
+{
+  idle_notifier = wayland_iface_register(
+          ext_idle_notifier_v1_interface.name, 1, 1,
+          &ext_idle_notifier_v1_interface);
+  if(!idle_notifier)
+    return FALSE;
+  vm_func_add("IdleTimeout", idle_timeout_action, TRUE);
+  idle_timers = g_hash_table_new_full((GHashFunc)str_nhash, (GCompareFunc)str_nequal,
+      (GDestroyNotify)idle_notification_free, NULL);
+  return TRUE;
+}
