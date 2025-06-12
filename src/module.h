eea0bd5339b93f125fd3e110af885ed4b1be8587
Lev Babiev <harley@hosers.org>
2025-06-12 07:46:52 +0100

    module: deprecate module queues

--- src/module.h
+++ src/module.h
@@ -6,18 +6,6 @@
 
 #define MODULE_API_VERSION 3
 
-typedef struct _module_queue {
-  GList *list;
-  GMutex mutex;
-  void *(*duplicate) (void *);
-  void (*free) (void *);
-  void *(*get_str) (void *, gchar *);
-  void *(*get_num) (void *, gchar *);
-  gboolean (*compare) (const void *, const void *);
-  const gchar *trigger;
-  gint limit;
-} module_queue_t;
-
 typedef void (*ModuleInvalidator)( void );
 typedef gboolean (*ModuleInitializer)( void );
 
@@ -35,11 +23,6 @@ typedef struct {
   GList *list;
   ModuleInterfaceV1 *active;
 } ModuleInterfaceList;
-
-void module_queue_append ( module_queue_t *queue, void *item );
-void module_queue_remove ( module_queue_t *queue );
-void *module_queue_get_string ( module_queue_t *queue, gchar *param );
-void *module_queue_get_numeric ( module_queue_t *queue, gchar *param );
 
 gboolean module_load ( gchar *name );
 void module_invalidate_all ( void );
