3fe791520aa0ed8aa5f0f320d1e6a7f810dd3839
Lev Babiev <harley@hosers.org>
2025-06-28 12:52:06 +0100

    actionlib: implement Print action

--- src/actionlib.c
+++ src/actionlib.c
@@ -710,6 +710,16 @@ static value_t action_update_widget ( vm_t *vm, value_t p[], gint np )
   return value_na;
 }
 
+static value_t action_print ( vm_t *vm, value_t p[], gint np )
+{
+  vm_param_check_np(vm, np, 1, "Print");
+  vm_param_check_string(vm, p, 0, "Print");
+
+  g_message("%s", value_get_string(p[0]));
+
+  return value_na;
+}
+
 void action_lib_init ( void )
 {
   vm_func_add("exec", action_exec_impl, TRUE);
@@ -754,4 +764,5 @@ void action_lib_init ( void )
   vm_func_add("DbusCallSession", action_dbus_call_session, TRUE);
   vm_func_add("exit", action_exit, TRUE);
   vm_func_add("UpdateWidget", action_update_widget, TRUE);
+  vm_func_add("Print", action_print, TRUE);
 }
